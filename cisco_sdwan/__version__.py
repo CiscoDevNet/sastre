fb45b4a43545d929566424bdb5fee814e8baa506
Marcelo Reis <mareis@cisco.com>
2023-03-13 14:13:04 -0400

    Updated version tag

--- cisco_sdwan/__version__.py
+++ cisco_sdwan/__version__.py
@@ -3,4 +3,4 @@ Sastre - Cisco-SDWAN Automation Toolset
 
 """
 __copyright__ = "Copyright (c) 2019-2023 Cisco Systems, Inc. and/or its affiliates"
-__version__ = "1.21.1_dev"
+__version__ = "1.21.1"
